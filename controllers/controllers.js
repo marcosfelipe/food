ec4af270dd6db70e2a42859fc8768e4230b5e115
marcosdebian <marcos.felype@hotmail.com>
2015-06-28 12:31:21 -0300

    fetching food from api, marking foods

--- controllers/controllers.js
+++ controllers/controllers.js
@@ -19,15 +19,37 @@ food.controller('RestaurantSelectCtrl', function ($rootScope, $scope, Restaurant
 food.controller('FoodSelectCtrl', function ($rootScope, $scope, FoodService) {
 
     FoodService.list($rootScope.restaurantId).success(function (data, status) {
+        angular.forEach(data.foods, function(food){
+            food.chose = false;
+            food.amount = 0;
+        });
         $scope.foods = data.foods;
     }).error(function(){
         location = '#/restaurant-select';
     });
 
     $scope.showDetail = function(data){
+        $scope.food_id = data.id;
         $scope.food_name = data.name;
         $scope.food_description = data.description;
         $('.ui.modal').modal('show');
+    };
+
+    $scope.check = function(food_id, amount){
+        angular.forEach($scope.foods, function(food){
+            if(food.id == food_id){
+                food.chose = true;
+                food.amount = amount;
+            }
+        });
+    };
+
+    $scope.uncheck = function(food_id){
+        angular.forEach($scope.foods, function(food){
+            if(food.id == food_id){
+                food.chose = false;
+            }
+        });
     };
 
 });
